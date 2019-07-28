d7f72925d7451de73e2e2cf96f8bcbc2bc4061a7
brendanlui <31925365+brendanlui@users.noreply.github.com>
2019-07-28 22:51:31 +0800

    1

---
+++ js/jquery.scrollTop.js
@@ -0,0 +1,1 @@
+$('a[href^="#"]').on('click', function(event) { var target = $(this.getAttribute('href')); if( target.length ) { event.preventDefault(); $('html, body').stop().animate({ scrollTop: target.offset().top }, 1000); } });
