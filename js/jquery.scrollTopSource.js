a0a6b0605cfe5435b8716fc039e806380a3cedbd
brendanlui <31925365+brendanlui@users.noreply.github.com>
2019-11-24 11:26:42 +0800

    1

--- js/jquery.scrollTopSource.js
+++
@@ -1,1 +0,0 @@
-$('a[href^="#toppestNav"]').on('click', function(event) { var target = $(this.getAttribute('href')); if( target.length ) { event.preventDefault(); $('#source').stop().animate({ scrollTop: 0 }, 1000); } });
